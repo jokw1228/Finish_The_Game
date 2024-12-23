d4dac7d1c216397119374b65785045062ae36240
jokw1228 <jokw1228@naver.com>
2024-12-23 10:45:32 +0900

    수 놓고 서브보드 골라서 돌리기 구현
    wait_choose_subboard, wait_choose_rotation

--- finish-the-game/scenes/game_pentago/pentago_board.gd
+++ finish-the-game/scenes/game_pentago/pentago_board.gd
@@ -41,8 +41,8 @@ func wait_put_stone(color_to_put: int = 1):
 	cell_image_size*board_size*subboard_size/2, \
 	cell_image_size*board_size*subboard_size/2\
 	)) / cell_image_size
-	select.x = int(select.x)
-	select.y = int(select.y)
+	select.x = floor(select.x)
+	select.y = floor(select.y)
 	if select.x < 0:
 		select.x = 0
 	elif select.x >= board_size*subboard_size:
@@ -54,7 +54,7 @@ func wait_put_stone(color_to_put: int = 1):
 		
 	# find the right subboard
 	var subboard_index: Vector2 = Vector2\
-	(int(select.x / subboard_size), int(select.y / subboard_size))
+	(floor(select.x / subboard_size), floor(select.y / subboard_size))
 	# find the index in that subboard
 	var inner_index: Vector2 = Vector2\
 	(int(select.x) % subboard_size, int(select.y) % subboard_size)
@@ -62,3 +62,46 @@ func wait_put_stone(color_to_put: int = 1):
 	var success: bool = subboards[subboard_index.y][subboard_index.x].put_stone(inner_index, color_to_put)
 	if success == false:
 		wait_put_stone(color_to_put)
+	else:
+		wait_choose_subboard()
+
+func wait_choose_subboard() -> void:
+	# waiting for a mouse left click
+	await get_tree().process_frame
+	while !Input.is_action_just_pressed("ui_left_click"):
+		await get_tree().process_frame
+	
+	# select: subboard index in this pentago board
+	var select: Vector2 = \
+	(get_global_mouse_position()\
+	 - position\
+	 + Vector2(\
+	cell_image_size*board_size*subboard_size/2, \
+	cell_image_size*board_size*subboard_size/2\
+	)) / (cell_image_size*subboard_size)
+	select.x = floor(select.x)
+	select.y = floor(select.y)
+	if select.x < 0:
+		select.x = 0
+	elif select.x >= board_size:
+		select.x = board_size - 1
+	if select.y < 0:
+		select.y = 0
+	elif select.y >= board_size:
+		select.y = board_size - 1
+	
+	wait_choose_rotation(select)
+
+func wait_choose_rotation(subboard_index: Vector2) -> void:
+	# waiting for a mouse left click
+	await get_tree().process_frame
+	while !Input.is_action_just_pressed("ui_left_click"):
+		await get_tree().process_frame
+	
+	var base_line_x: float = subboards[subboard_index.y][subboard_index.x].global_position.x
+	var click_x: float = get_global_mouse_position().x
+	
+	if click_x < base_line_x:
+		subboards[subboard_index.y][subboard_index.x].rotate_ccw()
+	else:
+		subboards[subboard_index.y][subboard_index.x].rotate_cw()
