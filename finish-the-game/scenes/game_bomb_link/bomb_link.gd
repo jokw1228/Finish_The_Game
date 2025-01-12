e594f4477cb5d2dba98e48183256ee284bfb77e5
jokw1228 <jokw1228@naver.com>
2025-01-12 17:39:19 +0900

    BombLink 폭탄 스프라이트 추가

--- finish-the-game/scenes/game_bomb_link/bomb_link.gd
+++ finish-the-game/scenes/game_bomb_link/bomb_link.gd
@@ -2,7 +2,7 @@ extends Node2D
 class_name BombLink
 
 const width = 4
-const height = 10
+const height = 10 # 게임오버되지 않는 선에서 폭탄이 존재 가능한 높이 + 1인 값임.
 
 var board: Array[Array] = []
 
@@ -15,3 +15,50 @@ func initialize_board() -> void:
 		for x: int in range(width):
 			temp.append(null)
 		board.append(temp)
+
+func insert_bomb_row_bottom(bomb_row_to_insert: Array[BombLinkBomb]) -> void:
+	var inserted_board: Array[Array] = []
+	for y: int in range(1, height):
+		var temp: Array[BombLinkBomb] = []
+		for x: int in range(width):
+			temp.append(board[y][x])
+		inserted_board.append(temp)
+	inserted_board.append(bomb_row_to_insert)
+	
+	board = inserted_board
+
+func append_bomb_row_top(bomb_row_to_append: Array[BombLinkBomb]) -> void:
+	var appended_board: Array[Array] = []
+	appended_board.append(bomb_row_to_append)
+	for y: int in range(1, height):
+		var temp: Array[BombLinkBomb] = []
+		for x: int in range(width):
+			temp.append(board[y][x])
+		appended_board.append(temp)
+	
+	board = appended_board
+	apply_gravity()
+
+func apply_gravity() -> void:
+	var flag: bool = true
+	while(flag == true):
+		var last_board: Array[Array] = board.duplicate(true)
+		
+		for y: int in range(height-2, -1, -1):
+			for x: int in range(width):
+				if board[y][x] != null:
+					# switch(BOMB_TYPE)
+					if board[y][x].bomb_type == BombLinkBomb.BOMB_TYPE.NORMAL \
+					or board[y][x].bomb_type == BombLinkBomb.BOMB_TYPE.NOT_ROTATABLE:
+						if board[y+1][x] != null:
+							board[y+1][x] = board[y][x]
+							board[y][x] = null
+		
+		flag = false
+		for y: int in range(height):
+			for x: int in range(width):
+				if board[y][x] != last_board[y][x]:
+					flag = true
+					break
+			if flag == true:
+				break
--- finish-the-game/scenes/game_bomb_link/bomb_link_bomb.gd
+++ finish-the-game/scenes/game_bomb_link/bomb_link_bomb.gd
@@ -9,6 +9,8 @@ enum BOMB_TYPE
 	GROUP_FOLLOWER
 }
 var bomb_type: BOMB_TYPE
+
+static var available_bomb_id: int = 1
 var bomb_group_id: int = -1
 
 enum FUSE_DIRECTION
@@ -23,8 +25,34 @@ var fuse_direction: FUSE_DIRECTION
 static func create(\
 bomb_type_to_set, \
 fuse_direction_to_set: FUSE_DIRECTION\
-) -> BombLinkBomb:
+) -> BombLinkBomb: # for NORMAL, NOT_ROTATABLE
 	var inst: BombLinkBomb = BombLinkBomb.new()
 	inst.bomb_type = bomb_type_to_set
 	inst.fuse_direction = fuse_direction_to_set
 	return inst
+
+static func generate_bomb_group_id() -> int:
+	var value: int = available_bomb_id
+	available_bomb_id += 1
+	return value
+
+static func create_group(\
+bomb_type_to_set, \
+bomb_group_id_to_set, \
+fuse_direction_to_set: FUSE_DIRECTION\
+) -> BombLinkBomb: # for GROUP_LEADER, GROUP_FOLLOWER
+	var inst: BombLinkBomb = BombLinkBomb.new()
+	inst.bomb_type = bomb_type_to_set
+	inst.bomb_group_id = bomb_group_id_to_set
+	inst.fuse_direction = fuse_direction_to_set
+	return inst
+
+func rotate_cw() -> void:
+	if fuse_direction == FUSE_DIRECTION.RIGHT:
+		fuse_direction = FUSE_DIRECTION.DOWN
+	elif fuse_direction == FUSE_DIRECTION.DOWN:
+		fuse_direction = FUSE_DIRECTION.LEFT
+	elif fuse_direction == FUSE_DIRECTION.LEFT:
+		fuse_direction = FUSE_DIRECTION.UP
+	elif fuse_direction == FUSE_DIRECTION.UP:
+		fuse_direction = FUSE_DIRECTION.RIGHT
