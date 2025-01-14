b6b30c375c03c360442aba6ea99b075cc8e93ab1
jokw1228 <jokw1228@naver.com>
2025-01-14 22:06:29 +0900

    FTGSlidingPuzzle에 타이머 추가

--- finish-the-game/scenes/game_utils/game_utils_game_timer.gd
+++ finish-the-game/scenes/game_utils/game_utils_game_timer.gd
@@ -3,9 +3,19 @@ class_name GameUtilsGameTimer
 
 signal timeout
 
+var tween_time: Tween
+
 func start_timer(duration: float):
 	visible = true
-	var tween_time: Tween = get_tree().create_tween()
+	tween_time = get_tree().create_tween()
 	tween_time.tween_property(self, "value", 0.0, duration)
 	await tween_time.finished
 	timeout.emit()
+
+func pause_timer():
+	if tween_time.is_running():
+		tween_time.pause()
+
+func resume_timer():
+	if tween_time.is_valid():
+		tween_time.play()
