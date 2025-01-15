a00319d1935654a467232d12aa17dc8392d8ca18
jokw1228 <jokw1228@naver.com>
2025-01-15 16:57:13 +0900

    BackgroundScrollingLine 신 추가

--- finish-the-game/scenes/background/background_scrolling_line.gd
+++ finish-the-game/scenes/background/background_scrolling_line.gd
@@ -1,10 +1,14 @@
 extends ColorRect
 class_name BackgroundScrollingLine
 
-var labels: Array[Label] = []
-
-var text: String = "test text"
-var duration: float = 3.0
+var label_text: String = "test text"
+var label_speed: float = 256.0
+var label_period: float = 1.8
+enum LABEL_DIRECTION {
+	LEFT,
+	RIGHT
+}
+var label_direction: LABEL_DIRECTION = LABEL_DIRECTION.RIGHT
 
 func _ready() -> void:
 	scrolling()
@@ -12,11 +16,16 @@ func _ready() -> void:
 func scrolling() -> void:
 	while true:
 		var inst: Label = Label.new()
-		inst.text = text
+		inst.text = label_text
 		add_child(inst)
 		
-		inst.position = Vector2(inst.size.x * (-1), 0)
-		var tween_position: Tween = get_tree().create_tween()
-		tween_position.tween_property(inst, "position", Vector2(1080, 0), duration)
+		if label_direction == LABEL_DIRECTION.LEFT:
+			inst.position = Vector2(size.x, 0)
+			var tween_position: Tween = get_tree().create_tween()
+			tween_position.tween_property(inst, "position", Vector2(-inst.size.x, 0), size.x / label_speed)
+		elif label_direction == LABEL_DIRECTION.RIGHT:
+			inst.position = Vector2(-inst.size.x, 0)
+			var tween_position: Tween = get_tree().create_tween()
+			tween_position.tween_property(inst, "position", Vector2(size.x, 0), size.x / label_speed)
 		
-		await tween_position.finished
+		await get_tree().create_timer(label_period).timeout
