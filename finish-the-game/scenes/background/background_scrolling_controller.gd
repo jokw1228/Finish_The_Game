1ccbc90e9f647f5eca29544931e7d3408c2fd40e
jokw1228 <jokw1228@naver.com>
2025-01-15 17:14:09 +0900

    misc

--- finish-the-game/scenes/background/background_scrolling_controller.gd
+++ finish-the-game/scenes/background/background_scrolling_controller.gd
@@ -1,2 +1,4 @@
 extends Control
 class_name BackgroundScrollingController
+
+@export var scrolling_lines: Array[BackgroundScrollingLine]
--- finish-the-game/scenes/background/background_scrolling_line.gd
+++ finish-the-game/scenes/background/background_scrolling_line.gd
@@ -1,6 +1,8 @@
 extends ColorRect
 class_name BackgroundScrollingLine
 
+var generating: bool = true
+
 var label_text: String = "test text"
 var label_speed: float = 256.0
 var label_period: float = 1.8
@@ -11,10 +13,11 @@ enum LABEL_DIRECTION {
 var label_direction: LABEL_DIRECTION = LABEL_DIRECTION.RIGHT
 
 func _ready() -> void:
+	await get_tree().create_timer(0.5).timeout
 	scrolling()
 
 func scrolling() -> void:
-	while true:
+	while generating:
 		var inst: Label = Label.new()
 		inst.text = label_text
 		add_child(inst)
@@ -29,3 +32,21 @@ func scrolling() -> void:
 			tween_position.tween_property(inst, "position", Vector2(size.x, 0), size.x / label_speed)
 		
 		await get_tree().create_timer(label_period).timeout
+
+func set_label_text(label_text_to_set: String) -> void:
+	label_text = label_text_to_set
+
+func set_label_speed(label_speed_to_set: float) -> void:
+	label_speed = label_speed_to_set
+
+func set_label_period(label_period_to_set: float) -> void:
+	label_period = label_period_to_set
+
+func set_label_direction(label_direction_to_set: LABEL_DIRECTION) -> void:
+	label_direction = label_direction_to_set
+
+func toggle_label_direction() -> void:
+	if label_direction == LABEL_DIRECTION.LEFT:
+		set_label_direction(LABEL_DIRECTION.RIGHT)
+	elif label_direction == LABEL_DIRECTION.RIGHT:
+		set_label_direction(LABEL_DIRECTION.LEFT)
