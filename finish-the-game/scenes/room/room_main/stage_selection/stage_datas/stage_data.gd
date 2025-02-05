a24483640cf8105f5b76d5e143b0f4552f9cbfe5
jokw1228 <jokw1228@naver.com>
2025-02-05 21:45:39 +0900

    펜타고, 슬라이딩 퍼즐 섬네일 추가

--- finish-the-game/scenes/room/room_main/stage_selection/stage_datas/stage_data.gd
+++ finish-the-game/scenes/room/room_main/stage_selection/stage_datas/stage_data.gd
@@ -1,7 +1,7 @@
 extends Resource
 class_name StageData
 
-@export var stage_name: String = ""
+@export_multiline var stage_name: String = ""
 @export_multiline var stage_description: String = ""
 
 @export var stage_thumbnail: Texture2D
