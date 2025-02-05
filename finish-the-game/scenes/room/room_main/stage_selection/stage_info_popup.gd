3385d73075687c9246eacfc0db28587dedff65c5
jokw1228 <jokw1228@naver.com>
2025-02-06 00:09:29 +0900

    팝업창 열고 닫기 구현

--- finish-the-game/scenes/room/room_main/stage_selection/stage_info_popup.gd
+++ finish-the-game/scenes/room/room_main/stage_selection/stage_info_popup.gd
@@ -4,13 +4,17 @@ class_name StageInfoPopup
 @export var stage_info_texture: TextureRect
 var stage_infos: Array[Texture2D] = []
 
-signal stage_info_exit_button_pressed()
-func _on_stage_info_exit_button_pressed() -> void:
-	stage_info_exit_button_pressed.emit()
+var is_popped_up: bool = false
 
 signal stage_info_button_pressed()
 func _on_stage_info_button_pressed() -> void:
-	stage_info_button_pressed.emit()
+	if is_popped_up == false:
+		stage_info_button_pressed.emit()
+
+signal stage_info_exit_button_pressed()
+func _on_stage_info_exit_button_pressed() -> void:
+	if is_popped_up == true:
+		stage_info_exit_button_pressed.emit()
 
 func receive_request_set_stage_datas(stage_datas_to_set: Array[StageData]) -> void:
 	initialize(stage_datas_to_set)
@@ -27,3 +31,19 @@ func receive_current_stage_has_been_changed(changed_current_stage: int) -> void:
 
 func update_stage_info(changed_current_stage: int) -> void:
 	stage_info_texture.texture = stage_infos[changed_current_stage]
+
+func receive_stage_selection_state_has_been_changed(changed_state: StageSelection.StageSelectionState) -> void:
+	if changed_state == StageSelection.StageSelectionState.STAGE_INFO_POPPED_UP \
+	and is_popped_up == false:
+		is_popped_up = true
+		open_popup()
+	elif changed_state == StageSelection.StageSelectionState.STAGE_SCROLLING \
+	and is_popped_up == true:
+		is_popped_up = false
+		close_popup()
+
+func open_popup() -> void:
+	visible = true
+
+func close_popup() -> void:
+	visible = false
--- finish-the-game/scenes/room/room_main/stage_selection/stage_selection.gd
+++ finish-the-game/scenes/room/room_main/stage_selection/stage_selection.gd
@@ -1,7 +1,12 @@
 extends Control
 class_name StageSelection
 
-var is_ready_to_get_input: bool = false
+enum StageSelectionState {
+	NO,
+	STAGE_SCROLLING,
+	STAGE_INFO_POPPED_UP
+}
+var stage_selection_state: StageSelectionState = StageSelectionState.NO
 
 var current_stage_index: int = 0
 @export var stage_datas: Array[StageData] = []
@@ -14,13 +19,17 @@ func _ready() -> void:
 	update_stage_name()
 	request_set_stage_datas.emit(stage_datas)
 
+signal stage_selection_state_has_been_changed(changed_state: StageSelectionState)
+func set_state(state_to_set: StageSelectionState) -> void:
+	stage_selection_state = state_to_set
+	stage_selection_state_has_been_changed.emit(stage_selection_state)
+
 func receive_player_started_game() -> void:
-	is_ready_to_get_input = true
+	set_state(StageSelectionState.STAGE_SCROLLING)
 	slide_in()
 
 func slide_in() -> void:
 	var target_position = Vector2(0, 0)
-
 	create_tween().tween_property(self, "position", target_position, 0.5)\
 	.set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
 
@@ -33,3 +42,11 @@ func receive_current_stage_has_been_changed(changed_current_stage: int) -> void:
 func update_stage_name() -> void:
 	stage_name.text = stage_datas[current_stage_index].stage_name
 	stage_description.text = stage_datas[current_stage_index].stage_description
+
+func receive_stage_info_button_pressed() -> void:
+	if stage_selection_state == StageSelectionState.STAGE_SCROLLING:
+		set_state(StageSelectionState.STAGE_INFO_POPPED_UP)
+
+func receive_stage_info_exit_button_pressed() -> void:
+	if stage_selection_state == StageSelectionState.STAGE_INFO_POPPED_UP:
+		set_state(StageSelectionState.STAGE_SCROLLING)
