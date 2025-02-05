0aed46896714fa2f87f9cb0a7c5d01bc7739339f
jokw1228 <jokw1228@naver.com>
2025-02-05 15:36:07 +0900

    스테이지 선택 스크롤러 임시 구현

---
+++ finish-the-game/scenes/room/room_main/utils/stage_scroll_container.gd
@@ -0,0 +1,40 @@
+extends ScrollContainer
+class_name StageScrollContainer
+
+var stage_count: int
+var stage_width: float
+
+@export var stage_container: HBoxContainer
+
+func _ready() -> void:
+	stage_count = stage_container.get_child_count()
+	if stage_count > 0:
+		stage_width = stage_container.get_child(0).size.x
+	else:
+		stage_width = 0
+	
+	get_h_scroll_bar().size.y = 320
+
+func _gui_input(event: InputEvent) -> void:
+	'''
+	if event is InputEventScreenTouch and not event.is_pressed():
+		snap_to_nearest_stage()
+	'''
+	if event is InputEventMouseButton and not event.is_pressed():
+		snap_to_nearest_stage()
+
+var tween_snap: Tween
+func snap_to_nearest_stage() -> void:
+	if stage_width == 0:
+		return
+	
+	var target_stage: int = int(round(scroll_horizontal / stage_width))
+	target_stage = clamp(target_stage, 0, stage_count - 1)
+	var target_scroll: float = target_stage * stage_width
+	
+	if tween_snap != null:
+		tween_snap.kill()
+	tween_snap = get_tree().create_tween()
+	tween_snap.tween_property(self, "scroll_horizontal", target_scroll, 0.3)\
+	.set_trans(Tween.TRANS_CUBIC)\
+	.set_ease(Tween.EASE_OUT)
