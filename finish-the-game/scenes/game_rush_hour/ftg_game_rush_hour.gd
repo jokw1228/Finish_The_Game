a85a58591cc87587850e3b5f61e61abc79fe7819
jokw1228 <jokw1228@naver.com>
2025-02-10 19:26:52 +0900

    러시아워 난이도 조정

--- finish-the-game/scenes/game_rush_hour/ftg_game_rush_hour.gd
+++ finish-the-game/scenes/game_rush_hour/ftg_game_rush_hour.gd
@@ -98,23 +98,23 @@ func _ready():
 	
 func set_difficulty(difficulty):
 	if difficulty < 0.2:
-		num_moves = randi_range(3,5)
+		num_moves = randi_range(3, 5)
 		time_limit = 12
 	
 	elif difficulty < 0.4:
-		num_moves = 6
+		num_moves = randi_range(5, 7)
 		time_limit = 11
 	
 	elif difficulty < 0.6:
-		num_moves = 8
+		num_moves = randi_range(7, 9)
 		time_limit = 10
 	
 	elif difficulty < 0.8:
-		num_moves = 10
+		num_moves = randi_range(9, 11)
 		time_limit = 9
 	
 	elif difficulty >= 0.8:
-		num_moves = 10
+		num_moves = randi_range(9, 11)
 		time_limit = 9 - (difficulty - 0.8) * 10
 	
 
