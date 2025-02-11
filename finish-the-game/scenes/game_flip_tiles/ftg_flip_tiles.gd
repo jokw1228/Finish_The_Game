726811ca779e10f2cfe94feca1ebc3c777549302
jokw1228 <jokw1228@naver.com>
2025-02-12 01:23:36 +0900

    타일 뒤집기 밸런스 패치

--- finish-the-game/scenes/game_flip_tiles/ftg_flip_tiles.gd
+++ finish-the-game/scenes/game_flip_tiles/ftg_flip_tiles.gd
@@ -13,7 +13,6 @@ signal pause_timer()
 
 func start_ftg(difficulty: float) -> void:
 	
-	print("apapapapapapp")
 	var ui = get_node("FlipTilesUi")
 	
 	var flip_tile_count: int = 4
@@ -22,28 +21,28 @@ func start_ftg(difficulty: float) -> void:
 	
 	if difficulty < 0.2:
 		flip_tile_count = 2
-		board_size = 3
-		time_limit = 9.0
+		board_size = 5
+		time_limit = 8.0
 	
 	elif difficulty < 0.4:
-		flip_tile_count = 2
-		board_size = 4
-		time_limit = 9.0
+		flip_tile_count = 3
+		board_size = 5
+		time_limit = 10.0
 	
 	elif difficulty < 0.6:
 		flip_tile_count = 3
 		board_size = 4
-		time_limit = 9.0
+		time_limit = 12.0
 	
 	elif difficulty < 0.8:
-		flip_tile_count = 3
-		board_size = 5
-		time_limit = 9.0
+		flip_tile_count = 4
+		board_size = 4
+		time_limit = 14.0
 	
 	elif difficulty >= 0.8:
-		flip_tile_count = 3
-		board_size = 5
-		time_limit = 9.0 - (difficulty - 0.8) * 10
+		flip_tile_count = 4
+		board_size = 4
+		time_limit = 14.0 - (difficulty - 0.8) * 10
 	
 	
 	ui.set_board_size(board_size, board_size)
