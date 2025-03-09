ac068fd8c550dcd2a80aa508caca051f85243d46
jokw1228 <jokw1228@naver.com>
2025-03-09 12:02:55 +0900

    윈도우 빌드

---
+++ finish-the-game/scenes/room/room_main/stage_datas/stage_data.gd
@@ -0,0 +1,11 @@
+extends Resource
+class_name StageData
+
+@export_multiline var stage_name: String = ""
+@export_multiline var stage_description: String = ""
+@export_multiline var stage_code: String = ""
+
+@export var stage_thumbnail: Texture2D
+@export var stage_info: Texture2D
+
+@export var stage_room: String
